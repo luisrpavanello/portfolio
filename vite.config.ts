080b7ebe7d068511532ab9a355e0fe9dfb228e55
Luis Renan Pavanello <102007776+LRPavanello@users.noreply.github.com>
2025-05-04 19:40:14 -0300

    Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', 
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  base: '/portfolio/',
 });
